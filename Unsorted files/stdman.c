ceb6f95df2c565625bb1afd2b2cece62d77e83c2
gitduck6 <>
2025-08-27 14:07:42 +0400

    infopriting added

--- Unsorted files/stdman.c
+++ Unsorted files/stdman.c
@@ -2,9 +2,10 @@
 making a student managing tool, using structs. 
 i want this to be a little more long  term than others projects of mine.
 objectives:
-    utilize structs []
-    Student definer []
+    utilize structs [done]
+    Student definer [done]
     student look up []
+    update students
 maybes:
     save to non voliatile memory []
     Compare students []
@@ -20,6 +21,7 @@ struct Student
     char name[MAXLEN + 1];
     int age;
     float gpa;
+    int id;
 };
 
 struct Student * stdlist = NULL;
@@ -41,13 +43,15 @@ void createStud(const char * name,int age,float gpa) {
             return;
         }
     } else {
-        struct Student * temp = realloc(stdlist,(studn + 1) * sizeof(struct Student));
+        struct Student * temp = realloc(stdlist,(studn) * sizeof(struct Student));
         if (temp != NULL) {
             stdlist = temp;
+            //free(temp); destroys our data ;o; DONT DO IT (took me some time to realize)
         } else {
             //error handling
             studn--;
             perror("Memory allocation failuire\n");
+            return;
 
         }
     }
@@ -56,10 +60,14 @@ void createStud(const char * name,int age,float gpa) {
     strcpy(stdlist[studn - 1].name, name); // just sets our input array into the uhh heap
     stdlist[studn - 1].age = age;
     stdlist[studn - 1].gpa = gpa;
-
-
+    stdlist[studn - 1].id = studn - 1;
     return;
 }   
+
+void printInfo(int studID) {
+    struct Student currentStud = stdlist[studID];
+    printf("UserID: %d\nName: %s\nAge: %d\nGPA: %.2f\n",studID,currentStud.name,currentStud.age,currentStud.gpa);
+}
 
 int main(int argc, char ** argv) {
     /*struct Student s1;
@@ -68,6 +76,8 @@ int main(int argc, char ** argv) {
     createStud("Red",21,6.7);
     createStud("Jimdy",19,3.5);
     printf("%s is %d years old and his/her gpa is %.2f\n",stdlist[1].name,stdlist[1].age,stdlist[1].gpa);
+    //printf("%d",sizeof(struct Student));
+    printInfo(1);
     free(stdlist);
     return 0;
 }
