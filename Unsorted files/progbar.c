509ff860eef76c84cd112c23ef2344bcd7ad76dc
gitduck6 <>
2025-09-19 23:50:48 +0400

    a functional portable timer

--- Unsorted files/progbar.c
+++ Unsorted files/progbar.c
@@ -1,45 +1,24 @@
 //progbar.c
 #include <stdio.h>
 #include <stdlib.h>
-
 #if defined(_WIN32) || defined(_WIN64)
 	#include <windows.h> 
-	char os_type = 0;
 	//time for windows systems
-#elif defined(__unix__) || defined(__unix) || defined(__APPLE__) && defined(__MACH__)
+#elif defined(__unix__) || defined(__unix) || ( defined(__APPLE__) && defined(__MACH__) )
 	#include <unistd.h>
-	char os_type = 1;
-
 	//time for unix systems
-#else 
-	#include <time.h>
-	char os_type = 2;
-	//the more portable but inefficent one for unsupported systems
 #endif
-/* 
-the os_type variable is just so i can know the operating system later
-	windows - 0
-	unix - 1
-	other - 2
-*/
 #define LIM 20
 
 void portWait(int secs) {
-	switch (os_type)
-	{
-	case 0:
-		Sleep(1000*secs);
-		break;
-	case 1:
-		sleep(secs);
-		break;
-	case 2:
-		clock_t start_time = clock();
-		while ((double)(clock() - start_time) / CLOCKS_PER_SEC < secs);
-		break;
-	default:
-		break;
-	}
+#if defined(_WIN32) || defined(_WIN64)
+	Sleep(1000 * secs);
+	// time for windows systems
+#elif defined(__unix__) || defined(__unix) || ( defined(__APPLE__) && defined(__MACH__) )
+	sleep(secs);
+	// time for unix systems
+#endif
+	
 }
 
 void printState(double ratio){
@@ -61,7 +40,7 @@ int main(int argc, char **argv){
 		fprintf(stderr,"Usage: %s [time]\n",argv[0]);
 		return 1;
 	}
-	int dur = atoi(argv[1]) - 1;
+	int dur = atoi(argv[1]);
 	int timer = 0;
 	fflush(stdout);
 
