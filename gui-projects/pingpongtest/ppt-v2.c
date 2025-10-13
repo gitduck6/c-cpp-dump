1263b0273bd35135e076c0c9057bd17da7a42e7c
gitduck6 <warmsot19@gmail.com>
2025-10-13 18:01:07 +0400

    pingpong v2

---
+++ gui-projects/pingpongtest/ppt-v2.c
@@ -0,0 +1,16 @@
+#include "raylib.h"
+/**\
+    * Due to the previous versions suboptimality ive decided to remake it.
+    * And to be honest, i pretty much mixed up so many things i think it would be better off if i remade it.
+    *
+    * 
+    * 
+    * 
+\**/
+
+int main(void) 
+{
+
+
+
+}
