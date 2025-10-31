179c044f5e6892e374b33b5e4935c5c0cc2a1d77
gitduck6 <warmsot19@gmail.com>
2025-10-31 23:08:36 +0400

    addition of cJson, will modularise later

--- gui-projects/weather-app/citytocord.c
+++ gui-projects/weather-app/citytocord.c
@@ -1,18 +1,44 @@
 #include "curl/curl.h"
 #include "string.h"
+#include "cJSON.h"
 
 /*
     * This is the first part of the weather app i decided to code
     * it takes the name of the city and returns its longtitude and latitude 
     * (WE failing in life with this one)
-    *
-    * 
-    * 
+    *  
 */
 
 size_t write_callback(void *contents,size_t size, size_t nmemb,void *UNUSED)
 {
-    fwrite(contents,size,nmemb,stdout);
+    cJSON *root = cJSON_Parse(contents);
+    if (root == NULL)
+    {
+        fprintf(stderr,"cjson failure : 1\n");
+        return 0;
+    }
+    cJSON *results = cJSON_GetObjectItem(root,"results");
+    if (results == NULL)
+    {
+        fprintf(stderr,"cjson failure : 2\n");
+        return 0;
+    }
+
+    cJSON *firstResult = cJSON_GetArrayItem(results,0);
+    if (!cJSON_IsObject(firstResult))
+    {
+        fprintf(stderr,"Not an object\n");
+        return 1;
+    }
+
+    cJSON *lat = cJSON_GetObjectItem(firstResult, "latitude");
+    cJSON *lon = cJSON_GetObjectItem(firstResult, "longitude");
+
+    if (cJSON_IsNumber(lon) && cJSON_IsNumber(lat))
+    {
+        printf("%f %f\n",lat->valuedouble,lon->valuedouble);
+    }
+    cJSON_Delete(root);
     return size * nmemb;
 }
 
@@ -24,14 +50,15 @@ void cityToCord(char *city)
         return;
     }
 
-
     int urlSize = 150;
     char *URL = malloc(urlSize * sizeof(char));
+    char *URLprefix = "https://geocoding-api.open-meteo.com/v1/search?name=";
+    char *URLpostfix = "&count=1";
 
-    // Concentrate the url
-    strcpy(URL,"https://geocoding-api.open-meteo.com/v1/search?name=");
-    strncat(URL,city, urlSize - strlen(URL) - 1);
-    strncat(URL,"&count=1", urlSize - strlen(URL) - 1);
+    // "construct" the url
+    strcpy(URL, URLprefix);
+    strncat(URL, city, urlSize - strlen(URL) - 1);
+    strncat(URL, URLpostfix, urlSize - strlen(URL) - 1);
 
     CURL *curl = NULL;
     CURLcode res;
---
+++ gui-projects/weather-app/citytocord.h
@@ -0,0 +1,6 @@
+#pragma once
+#include <stdio.h>
+
+size_t write_callback(void *contents,size_t size, size_t nmemb,void *UNUSED);
+
+void cityToCord(char *city);
