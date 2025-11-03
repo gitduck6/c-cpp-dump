af997477ac2c4bdf1d1a675a4c166db17dd63bf2
gitduck6 <warmsot19@gmail.com>
2025-11-03 12:51:17 +0400

    Unix compatability

--- Unsorted files/insinput.c
+++ Unsorted files/insinput.c
@@ -1,8 +1,32 @@
 #include <stdio.h>
-#include <conio.h>
+#if defined(__WIN32)
+    #include <conio.h>
+
+#else
+    #include <unistd.h>
+    #include <termios.h>
+char getch(void)
+{
+    struct termios oldattr, newattr;
+    char ch;
+
+    tcgetattr(STDIN_FILENO,&oldattr);
+    newattr = oldattr;
+    newattr.c_lflag &= ~(ECHO | ICANON);
+    tcsetattr(STDIN_FILENO,TCSANOW,&newattr);
+    ch = getchar();
+    tcsetattr(STDIN_FILENO,TCSANOW,&oldattr);
+    return ch;
+}
+#endif
 /*
     * instant input in the windows OS
+    *
+    * Now i will try toi make it "unix portable" i guess
+    * CORRECTION : Me and my bestfriend chatgpt
+    * Also testing might be faulty since i myself am not on a unixlike system
     * 
+    * damn its actually almost slightly not too complicated, just a little tho
 */
 
 int main(void)
