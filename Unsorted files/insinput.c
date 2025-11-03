63aaff420638a2c95d89ea3d1450bcebfd4fb10e
gitduck6 <warmsot19@gmail.com>
2025-11-03 13:34:18 +0400

    extra documentation

--- Unsorted files/insinput.c
+++ Unsorted files/insinput.c
@@ -1,22 +1,30 @@
 #include <stdio.h>
-#if defined(__WIN32)
+#if defined(__WIN32) || defined(_WIN32)
     #include <conio.h>
 
 #else
+// basically, if we arent in windows do these:
     #include <unistd.h>
     #include <termios.h>
 char getch(void)
 {
-    struct termios oldattr, newattr;
-    char ch;
+    // the variables:
+    struct termios oldattr, newattr; //to store the new and original "config files"
+    char ch; // what we'll return
 
-    tcgetattr(STDIN_FILENO,&oldattr);
+    tcgetattr(STDIN_FILENO,&oldattr);// get the config thing of stdin and store it in oldattr
     newattr = oldattr;
     newattr.c_lflag &= ~(ECHO | ICANON);
-    tcsetattr(STDIN_FILENO,TCSANOW,&newattr);
-    ch = getchar();
-    tcsetattr(STDIN_FILENO,TCSANOW,&oldattr);
-    return ch;
+    /*
+        * bitwise operator magic ~ is the NOT operator
+        * so 1010 turns into 0101, or something like that, then & turns off those bits
+        * turning off ECHO stops characters we type from being seen
+        * while ICANON makes the characters get inputted instantly, without a NEWLINE
+    */
+    tcsetattr(STDIN_FILENO,TCSANOW,&newattr); //sets the new attribute
+    ch = getchar();// getchar's with the new configuration
+    tcsetattr(STDIN_FILENO,TCSANOW,&oldattr);// goes back to the previous config
+    return ch;//returns our char
 }
 #endif
 /*
@@ -27,6 +35,14 @@ char getch(void)
     * Also testing might be faulty since i myself am not on a unixlike system
     * 
     * damn its actually almost slightly not too complicated, just a little tho
+    * 
+    * ima try to explain now
+    * SO conio does what i want with getch, but the problem is that its only in windows
+    * therefore you gotta make ur own getch for unix, which introduces things ive neverseen
+    * such as the termios header
+    * 
+    * Okay so it works in an ubuntu vm i hope it works everywhere else too
+    * 
 */
 
 int main(void)
