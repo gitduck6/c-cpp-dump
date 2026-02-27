9b9b89d5f80895a1e1186280874a0ca9818fa450
gitduck6 <warmsot19@gmail.com>
2026-02-28 00:22:37 +0400

    Allowed multiple file arguments and added comments

--- Unsorted files/catclone2.c
+++ Unsorted files/catclone2.c
@@ -1,18 +1,34 @@
 #include <stdio.h>
 
+/*
+    * i think i would like to come back to this, i will try to remake a util everyday
+    * that would make this day 1?
+    * and i think my goal for now is to make a chat server, "THE BIG GOAL" in a way
+    * 
+    * but i do not intend to stop there ofc :)
+    *
+*/
 int main(int argc, char ** argv)
 {
-    int c;
-    FILE * fp = fopen(argv[1],"r");
-    if (fp == NULL)
-    {
-        perror("file");
-        return 1;
-    }
 
-    while ((c = fgetc(fp)) != EOF)
+    // Goes through every index of argv from 1 till argc-1
+    for (int i = 1;i < argc;i++)
     {
-        fputc((char)c,stdout);
+        
+        int c;
+        FILE * fp = fopen(argv[i],"r");
+        if (fp == NULL)
+        {
+            perror("file");
+            return 1;
+        }
+
+        while ((c = fgetc(fp)) != EOF)
+        {
+            fputc((char)c,stdout);
+        }
+
     }
+    putchar('\n');
     return 0;
 }
