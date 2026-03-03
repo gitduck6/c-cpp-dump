fc29639a566bb87b69efd83996f162a7db5b61cd
gitduck6 <warmsot19@gmail.com>
2026-03-03 16:59:44 +0400

    Created ship.c

---
+++ Unsorted files/ship.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+/*
+    * This c program is a fun little utility to evaluate the "compatability" chance of 2 strings,
+    * presumabily names, presumabily of people.
+    * 
+    * My current idea is to always return the same value  for 2 different names
+    * and a little "goal" i currently have is to make my own "randomness function"
+    * 
+    * my idea of this randomness is inspired by a thing i saw some time ago
+    * the main process was to add c*(2^n) for every nth index of our strings (c being the value of every character), then sum OR multiply the answers
+    * after all this find its modul from a division to 100, and thats your "compatability chance"
+    * 
+    * while yes, this might cause buffer overflows we will try to use them for our advantage
+    * since theyre somewhat predictable. e.g.: UINT_MAX + 1 = 0
+    * i should mention that we will use unsigned integers
+    * 
+    * also i kinda missed day 4 didnt i?
+*/
+
+int main(void)
+{
+    char person1[32];
+    char person2[32];
+
+    unsigned int randn1 = 0;
+    unsigned int randn2 = 0;
+
+    printf("Please enter the name of the first person!\n");
+    fgets(person1,sizeof(person1) - 1,stdin);
+
+    printf("Please enter the name of the second person!\n");
+    fgets(person2,sizeof(person2) - 1,stdin);
+
+    for (int i = 0;person1[i] != '\0';i++)
+    {
+        randn1 += (person1[i] * (1 << i));
+    }
+    for (int i = 0;person2[i] != '\0';i++)
+    {
+        randn2 += (person2[i] * (1 << i));
+    }
+    
+    printf("%d %d\n",randn1,randn2);
+
+}
