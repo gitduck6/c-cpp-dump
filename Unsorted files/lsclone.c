46a563ae1e428605c80e0d48e5cc2bab8cdda112
gitduck6 <warmsot19@gmail.com>
2026-03-29 23:52:37 +0400

    Added comments and a minor change(i forgot what it was)

--- Unsorted files/lsclone.c
+++ Unsorted files/lsclone.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
 #include <dirent.h>
 
+/*
+    * Hello, this was just a quick "reimplementation" of the well-known ls shell utility
+    * i barely code anymore, and i notice how much my skills have worsened
+    * i hope i can fix it :)
+    * 
+    * anyway for now this program works only in unix-like systems
+    * i tried to add something with win32 ifdefs but some ls alternative i found on microsofts website was too complex
+    * so i just gave up on that
+    * atleast for today
+    * 
+    * the basic algorithm:
+    * if there are no arguments (excluding argv[0]), list the current directory (".")
+    * for every argument, you list that argument,
+    * and if the amount of arguments is larger than 1 (again excluding the programs name)
+    * you print the name of the file before listing
+    *
+*/
+
 void list_dir(char * dir_name)
 {
     DIR *dir; 
@@ -24,9 +42,10 @@ void list_dir(char * dir_name)
 int main(int argc, char** argv) 
 {
     if (argc == 1) list_dir(".");
-    
+
     for (int i = 1;i < argc;i++)
     {
+        if (argc > 2) printf("%s:\n",argv[i]);
         list_dir(argv[i]);
     }
 
