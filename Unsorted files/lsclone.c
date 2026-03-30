a2d2d6ad0dfb2962747d6e881a06c995bb50229e
gitduck6 <warmsot19@gmail.com>
2026-03-30 12:44:34 +0400

    Added Win32 support and ignored the hidden files

--- Unsorted files/lsclone.c
+++ Unsorted files/lsclone.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
-#include <dirent.h>
+
+#if defined(__unix) || defined(__unix__)
+    #include <dirent.h>
+#elif defined(_WIN32) || defined(_WIN64)
+    #include <windows.h>
+#endif
+
 
 /*
     * Hello, this was just a quick "reimplementation" of the well-known ls shell utility
@@ -16,11 +22,15 @@
     * for every argument, you list that argument,
     * and if the amount of arguments is larger than 1 (again excluding the programs name)
     * you print the name of the file before listing
+    * 
+    * Hellooo this is me in the next day, today i added win32 support and ignored the hidden files
+    * 
     *
 */
 
 void list_dir(char * dir_name)
 {
+#if defined(__unix) || defined(__unix__)
     DIR *dir; 
     struct dirent *dir_entry; 
     dir = opendir(dir_name);
@@ -33,10 +43,34 @@ void list_dir(char * dir_name)
 
     while ((dir_entry = readdir(dir)) != NULL)
     {
+        if (dir_entry->d_name[0] == '.') continue; //hidden file so we skip
         printf("%s\n",dir_entry->d_name);
     }
 
     closedir(dir);
+#elif defined(_WIN32) || defined(_WIN64)
+
+    WIN32_FIND_DATA file;
+    HANDLE file_handle = NULL;
+    char path[MAX_PATH];
+
+    snprintf(path,sizeof(path),"%s\\*",dir_name); // turn  filename into filename\* to list ALL files instead of the folder itself 
+
+    if ((file_handle = FindFirstFile(path, &file)) == INVALID_HANDLE_VALUE) 
+    {
+    fprintf(stderr, "Could not find specified directory!\n");
+    exit(1); 
+    }
+
+    do {
+    if (file.cFileName[0] == '.') continue;
+    printf("%s\n", file.cFileName);
+    } 
+    while (FindNextFile(file_handle, &file));
+
+    FindClose(file_handle);
+
+#endif
 }
 
 int main(int argc, char** argv) 
