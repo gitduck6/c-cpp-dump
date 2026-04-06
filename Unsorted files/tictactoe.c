3656401ae48ac0bde95e067dc3861e939b46d1a7
gitduck6 <warmsot19@gmail.com>
2026-04-06 23:57:11 +0400

    Terrible but functional input reader, goodnight

--- Unsorted files/tictactoe.c
+++ Unsorted files/tictactoe.c
@@ -1,21 +1,28 @@
 #include <stdio.h>
 
+void print_board(char buffer[4][4])
+{
+    for (int i = 0;i < 3;i++)
+    {
+        printf("%s\n",buffer[i]);
+    }
+}
 
 int init_game()
 {
-    char ttt_buffer[3][3] = {0};
+    char ttt_buffer[4][4] = {"   ", "   ", "   ", (char)0};
 
 
     while (1)
     {
-        char position[2];
+        int x = 0, y = 0;
 
         while (1) 
         {
         printf("Please enter the X position of your move 1-3:\n");
-        scanf("%d",position);
+        scanf("%d",&x);
 
-        if ((*position > 3) || (*position < 1))
+        if ((x > 3) || (x < 1))
             continue;
         else
             break;
@@ -24,15 +31,17 @@ int init_game()
         while (1) 
         {
         printf("Please enter the Y position of your move 1-3:\n");
-        scanf("%d",position + 1);
+        scanf("%d",&y);
 
-        if ((*(position + 1) > 3) || (*(position + 1) < 1))
+        if ((y > 3) || (y < 1))
             continue;
         else
             break;
         }
 
-        printf("%d %d\n",position[0],position[1]);
+        ttt_buffer[x-1][y-1] = 'x';
+        print_board(ttt_buffer);
+        
     }
 }
 
