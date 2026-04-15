425bb7da0462c54506f7cf37e51f1c511700cd3e
gitduck6 <warmsot19@gmail.com>
2026-04-15 15:17:17 +0400

    Added a function from insinput.c

--- Unsorted files/snakegame.c
+++ Unsorted files/snakegame.c
@@ -2,10 +2,33 @@
 #include <stdlib.h>
 #include <string.h>
 
+#if defined(__WIN32) || defined(_WIN32)
+    #include <conio.h>
+#else    
+    #include <termios.h>
+    #include <unistd.h>
+#endif
+
 #define BOARD_HEIGHT 16
 #define BOARD_WIDTH 16
 
 char **board;
+
+#if !(defined(__WIN32) || defined(_WIN32))
+char getch(void)
+{
+    struct termios oldattr, newattr;
+    char ch;
+
+    tcgetattr(STDIN_FILENO,&oldattr);
+    newattr = oldattr;
+    newattr.c_lflag &= ~(ECHO | ICANON);
+    tcsetattr(STDIN_FILENO,TCSANOW,&newattr);
+    ch = getchar();
+    tcsetattr(STDIN_FILENO,TCSANOW,&oldattr);
+    // i dont wanna re-document this so please check the insinput.c source :)
+}
+#endif
 
 void init_board()
 {
