1f913a2349c86235f1e52c30191802de454bb8e6
gitduck6 <warmsot19@gmail.com>
2026-04-23 16:06:20 +0400

    Added comments

--- Unsorted files/my_printf.c
+++ Unsorted files/my_printf.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
 #include <stdarg.h>
 
+/*
+    * minimal reimplementation of the printf stdio function for fun:
+    *
+    * This is actually alot simpler than i thougt and im very happy for that
+    * we simply go through the array when we see percentage sign
+    * (which is what format specifiers start with - %c %d etc)
+    * then take the next character and act accordingly
+    * for example treat the format argumetn thingy as a character in case of %c
+    * 
+    * By the way, these are called varadaic functions and this was my first time working with them
+    * so the code might be a little messy 
+    * 
+    * currently theres only %c and %%, ill try to add hex now
+    * 
+    *
+*/
+
 int my_prinf(const char * format, ...)
 {
     va_list va_data;
